308c2adc0d98f2fa92ef0bed144af9e3562b6cc6
k-Mounya <mounya.kazi@gmail.com>
2024-10-14 16:16:43 +0200

    Update Application_Projet_Energie.py

--- Application_Projet_Energie.py
+++ Application_Projet_Energie.py
@@ -16,7 +16,7 @@ pd.set_option('display.max_columns', None)
 dfsmp = pd.read_csv('dfsmp.csv', sep=',', header=0)
 
 if page == pages[0]:
-    st.write("## Contexte du projet")
+    st.write("### Contexte du projet")
     st.write("La croissance démographique, l’accès d’une part grandissante de la population mondiale à l’énergie, le développement rapide de certaines économies, synonyme d’industrialisation, les pays développés habitués à une énergie abondante et relativement bon marché, sont autant de facteurs contribuant à une hausse continue de la consommation d’énergie.")
     st.write("Le secteur économique de l'énergie en France comprend la production locale et l'importation d'énergie primaire, Pour couvrir les besoins énergétiques de la France, la branche énergétique française utilise de l'énergie primaire, produite en France ou importée, puis la transforme et la distribue aux utilisateurs.")
     st.write("Nous nous intéressons à la production locale, ainsi la France compte dans son bouquet énergétique des énergies fossiles et d’autres renouvelables tels que : le nucléaire, le pétrole, le gaz naturel, des d'énergies renouvelables et déchets.")
@@ -27,8 +27,8 @@ if page == pages[0]:
     st.write("- Un focus sur les énergies renouvelables et leurs lieux d’implantation.")
     st.write("- Pour y parvenir, nous allons utiliser un ensemble de données d’approximativement 2 millions d’enregistrements. Les données contiennent les informations sur la consommation d’électricité et sa production à partir de plusieurs de plusieurs sources d’énergie : nucléaire, solaire, éolienne, bioénergie, fioul, …  par région métropolitaine (hors corse) enregistrées par demi-heure.")
     st.image('Images/Image énergies.jpg')
+
 elif page == pages[1]:
-    st.write("## Exploration des données")
     st.write("Notre DataFrame sur les consommations d'énergie par région et par tranche de 3 h")
     pd.set_option('display.max_columns', None)
     dfsmp = pd.read_csv('dfsmp.csv', sep=',', header=0)
@@ -41,12 +41,7 @@ elif page == pages[1]:
 
         # Checkbox pour afficher les valeurs manquantes
     if st.checkbox('Afficher les années'):
-        #st.write(dfsmp['annee'].unique())
-        annees = dfsmp['annee'].unique()
-        annees_str = [str(annee) for annee in annees]
-        annees_df = pd.DataFrame(annees_str, columns=["Année"])
-        st.write(annees_df)
-         
+        st.write(dfsmp['annee'].unique())
     
     # Checkbox pour afficher le shape du DataFrame
     if st.checkbox('Afficher les colonnes et types'):
@@ -64,19 +59,16 @@ elif page == pages[1]:
         st.write(dfsmp.shape)
 
 elif page == pages[2]:
-    st.write("## Analyse des Données")
+    st.write("### Analyse des Données")
 
     # Prétraitement des données pour assurer le bon format des colonnes
     dfsmp['annee_mois'] = pd.to_datetime(dfsmp['annee_mois'], format='%Y-%m').dt.to_period('M').astype(str)
 
     # Création du pivot table pour la heatmap avec la température
-    
-    #Moon j'ai arrondi la tempréture à 2 chiffre en partie décimale pour une meilleure lisibilité
     heatmap_data = dfsmp.pivot_table(index='annee_mois', columns='region', values='temperature (C°)', aggfunc='mean')
-    heatmap_data = heatmap_data.round(2)
     st.write("Données de la Heatmap :")
     st.write(heatmap_data)
-    
+
     # Création de la heatmap avec Plotly
     fig = px.imshow(heatmap_data, 
                     labels=dict(x="Région", y="Année-Mois", color="Température (°C)"),
@@ -85,24 +77,10 @@ elif page == pages[2]:
                     color_continuous_scale='Viridis')  # Utilisation d'une échelle de couleurs contrastée
     
     st.plotly_chart(fig)    
-    #st.write ("On remarque que la moyenne de température en Provence-Alpes Côtes d'Azur est la plus forte de toutes les régions de France")
-    #st.write ("Ce sont les régions Hauts de France, Normandie et Bretagne, plus proches du littoral, qui ont les températures les plus faibles en moyenne sur toute la période observée")
-    #st.write ("Sans surprise, c'est bien pendant les mois d'hiver (Décembre, Janvier, Février) que l'on observe les températures les plus froides, en particulier pour le Grand-Est, plus éloigné du littoral")
-    #st.write ("******************************************************************************************************************")
-    st.write("### Analyse des températures régionales")
-    st.write("""
-    ### Provence-Alpes-Côte d'Azur : La région la plus chaude
-    La région **Provence-Alpes-Côte d'Azur** se distingue avec les températures moyennes les plus élevées en France. Cela s'explique par l'influence de son climat méditerranéen, caractérisé par des étés chauds et des hivers doux.
-    """)
-    st.write("""
-    ### Régions aux températures les plus basses
-    À l'opposé, les régions **Hauts-de-France**, **Normandie**, et **Bretagne**, situées sur le littoral nord-ouest, présentent les températures moyennes les plus faibles. Ces régions sont influencées par l'océan Atlantique et la Manche, ce qui entraîne des températures plus fraîches tout au long de l'année.
-    """)
-    st.write("""
-    ### Mois d'hiver : Les températures les plus froides
-    Sans surprise, les températures les plus basses sont observées durant les mois d'hiver (**Décembre, Janvier, Février**). Le **Grand-Est**, situé à l'intérieur des terres et loin du littoral, subit les plus fortes baisses de température en raison de son climat continental.
-    """)
-
+    st.write ("On remarque que la moyenne de température en Provence-Alpes Côtes d'Azur est la plus forte de toutes les régions de France")
+    st.write ("Ce sont les régions Hauts de France, Normandie et Bretagne, plus proches du littoral, qui ont les températures les plus faibles en moyenne sur toute la période observée")
+    st.write ("Sans surprise, c'est bien pendant les mois d'hiver (Décembre, Janvier, Février) que l'on observe les températures les plus froides, en particulier pour le Grand-Est, plus éloigné du littoral")
+    st.write ("******************************************************************************************************************")
     #############################################################################
     df = dfsmp.groupby('annee_mois').agg({
     'temperature (C°)': 'mean',
@@ -328,89 +306,129 @@ elif page == pages[2]:
     st.write (" Les régiosn qui produisent le plus d'énergie : Auvergne Rhône Alpes, Centre Val de Loire, Grand Est, Hauts de France, Normandie et Nouvelle Aquitaine")
     st.write ("******************************************************************************************************************")
     #############################################################
-#partie que j'ai changé ell est dans le bloc note
+
+
 elif page == pages[3]:
-    st.write("## Modélisation et Prédictions")
-
-# Créer des onglets pour diviser la page en plusieurs sections
-tab1, tab2, tab3 = st.tabs(["Résultats des modèles", "Features Importance", "Prédictions et Analyse"])
-
-# Premier onglet : Résultats des modèles
-with tab1:
-    st.write("### Résultats des modèles")
-
-    # Diviser en deux colonnes pour afficher côte à côte
-    col1, col2 = st.columns(2)
-
-    # --- Partie 1 : Premier tableau dans la première colonne ---
-    with col1:
-        st.write("#### Performances des modèles (Entraînement)")
-        result_models = pd.read_csv('result_models.csv', sep=';', header=0)
-        st.write(result_models)
-        
-        st.write("""
-        **Évaluation des Modèles d'Entraînement : Performances et Comparaison**
-
-        Le tableau ci-dessus présente les métriques d'évaluation de chaque modèle. Nous observons que le **Random Forest** se distingue clairement comme le meilleur modèle...
-        """)
-
-    # --- Partie 2 : Deuxième tableau ou image dans la deuxième colonne ---
-    with col2:
-        st.write("#### Optimisation des Modèles (Grid Search)")
-        st.image("Images/post-gridsearch.png")
-        
-        st.write("""
-        **Analyse des Résultats après Optimisation (Grid Search)**
-
-        Après avoir appliqué la méthode **Grid Search**, nous avons affiné les hyperparamètres des modèles non linéaires...
-        """)
-
-# Deuxième onglet : Feature Importance
-with tab2:
-    st.write("### Feature Importance")
+    st.write("### Modélisation et Prédictions")
     
-    st.write("#### Feature importance Random Forest")
-    st.image("Images/feature importance RandomForest.png")
+    # Afficher les résultats des modèles
+    pd.set_option('display.max_columns', None)
+    result_models = pd.read_csv('result_models.csv', sep=';', header=0)
+    st.write(result_models)
+
+    st.image(r"C:\Users\Mounia\Documents\Projet ENERGIE\Images\post-gridsearch.png")
+
+    # Charger les résultats sauvegardés
+    results_path = 'Modèles et résultats JOBLIB\LRresults.pkl'
+    results = joblib.load(results_path)
+    
+    #Afficher les features importances
+    st.title("Feature importance Random Forest")
+    st.image("Images\feature importance RandomForest.png")
   
-    st.write("#### Feature importance Decision Tree")
-    st.image("Images/feature importance DecisionTree.png")
+    st.title("Feature importance Decision Tree")
+    st.image("Images\feature importance DecisionTree.png")
 
-    st.write("#### Feature importance XGB")
-    st.image("Images/feature importance XGB.png")
+    st.title("Feature importance XGB")
+    st.image("Images\feature importance XGB.png")
 
-# Troisième onglet : Prédictions et Analyse
-with tab3:
-    st.write("### Prédictions et Analyse")
-    
-    # Ajout des visualisations et analyse
-    st.write("#### Shape de Random Forest Regressor")
-    st.image("Images/Shape Random Forest Regressor.png")
+    # Afficher les images et les explications
+    st.title("Shape de Random Forest Regressor")
+    st.image("Images\Shape Random Forest Regressor.png")
     st.write("On remarque que les variables ayant le plus d'impact dans le modèle Random Forest Regressor sont : population, bioen, therm, Température (C°)")
-
-    st.write("#### Shape de Decision Tree Regressor")
-    st.image("Images/Shape Decision Tree Regressor.png")
+    
+    st.title("Shape de Decision Tree Regressor")
+    st.image("Images\Shape Decision Tree Regressor.png")
     st.write("On remarque que les variables ayant le plus d'impact dans le modèle Decision Tree Regressor sont : population, therm, ech_phy")
 
-    st.write("#### Shape de XGB Regressor")
-    st.image("Images/Shape XGB Regressor.png")
+    st.title("Shape de XGB Regressor")
+    st.image("Images\Shape XGB Regressor.png")
     st.write("On remarque que les variables ayant le plus d'impact dans le modèle XGB Regressor sont : population, bioen, therm, Température (C°)")
-
-    st.write("### Prédictions par région")
-    st.image("Images/predic vs reel IDF.png")
+    
+    st.image("Images\predic vs reel IDF.png")
     st.write("Île-de-France (IDF) - région avec une forte densité de population et une demande énergétique importante.")
-    st.image("Images/predic vs reel PAC.png")
+    st.image("Images\predic vs reel PAC.png")
     st.write("Provence-Alpes-Côte d'Azur (PACA) - région plus ensoleillée et avec des variations de consommation différentes.")
-    st.image("Images/predic vs reel NAQ.png")
+    st.image("Images\predic vs reel NAQ.png")
     st.write("Nouvelle-Aquitaine (NAQ) - une région avec une répartition plus rurale et des besoins énergétiques différents.")
+
+    st.write("On remarque que les modèles suivent bien les tendances du réel. Tous sous-estiment les valeurs en semaine et surestiment les valeurs du week-end")
+
+    model_path = "Modèles et résultats JOBLIB\Random_Forest_Regressor_model.pkl"
+    model = joblib.load(model_path)
+    st.session_state.new_data = pd.DataFrame()
+    # Conversion de la colonne 'date_heure' en datetime sans format spécifié
+    dfsmp['date_heure'] = pd.to_datetime(dfsmp['date_heure'], errors='coerce')
     
-    st.write("On remarque que les modèles suivent bien les tendances du réel. Tous sous-estiment les valeurs en semaine et surestiment les valeurs du week-end.")
-    
-    # Sélection de la région et prédiction
+    # Obtenez les régions uniques
     regions = dfsmp['region'].unique()
+
+    # Sélection de la région
     selected_region = st.selectbox("Choisis une région", regions)
+    date_input = st.date_input("Date", min_value=datetime(2023, 1, 1), max_value=datetime(2100, 12, 31))
+    month = date_input.month  # Définir le mois après la sélection de la date
+     # Liste des tranches d'heures (chaque tranche de 3 heures)
+    time_slots = ["00:00", "03:00", "06:00", "09:00", "12:00", "15:00", "18:00", "21:00"]
+      # Sélection d'une heure avec des tranches de 3 heures
+    selected_time = st.selectbox("Choisis une heure (tranches de 3h)", time_slots)
+    # Convertir l'heure sélectionnée en nombre d'heures
+    hour = int(selected_time.split(":")[0])
     population = st.number_input("Population", min_value=0)
+    year=date_input.year
+    day=date_input.day
     
-    # Prédiction et affichage des résultats
+    # Filtrage des données
+    filtered_data = dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]
+    
+    new_data = pd.DataFrame({
+    'therm': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['therm'].mean(),
+    'nucl': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['nucl'].mean(),
+    'eol': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['eol'].mean(),
+    'sol': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['sol'].mean(),
+    'hydr': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['hydr'].mean(),
+    'pomp': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['pomp'].mean(),
+    'bioen': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['bioen'].mean(),
+    'ech_phy': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['ech_phy'].mean(),
+    'stock_bat': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['stock_bat'].mean(),
+    'destock_bat': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['destock_bat'].mean(),
+    'eol_terr': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['eol_terr'].mean(),
+    'eol_off': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['eol_off'].mean(),
+    'pression_niv_mer (Pa)': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['pression_niv_mer (Pa)'].mean(),
+    'vitesse du vent moyen 10 mn (m/s)': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['vitesse du vent moyen 10 mn (m/s)'].mean(),
+    'temperature (C°)': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['temperature (C°)'].mean(),
+    'humidite (%)': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['humidite (%)'].mean(),
+    'pression station (Pa)': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['pression station (Pa)'].mean(),
+    'precipitations dans les 3 dernieres heures (mm)': dfsmp[(dfsmp['region'] == selected_region) & (dfsmp['date_heure'].dt.month == month) & (dfsmp['date_heure'].dt.hour == hour)]['precipitations dans les 3 dernieres heures (mm)'].mean(),
+    'année': [year],
+    'population': [population],
+    'region_FR-ARA': [1 if selected_region == 'FR-ARA' else 0],
+    'region_FR-BFC': [1 if selected_region == 'FR-BFC' else 0],
+    'region_FR-BRE': [1 if selected_region == 'FR-BRE' else 0],
+    'region_FR-CVL': [1 if selected_region == 'FR-CVL' else 0],
+    'region_FR-GES': [1 if selected_region == 'FR-GES' else 0],
+    'region_FR-HDF': [1 if selected_region == 'FR-HDF' else 0],
+    'region_FR-IDF': [1 if selected_region == 'FR-IDF' else 0],
+    'region_FR-NAQ': [1 if selected_region == 'FR-NAQ' else 0],
+    'region_FR-NOR': [1 if selected_region == 'FR-NOR' else 0],
+    'region_FR-OCC': [1 if selected_region == 'FR-OCC' else 0],
+    'region_FR-PAC': [1 if selected_region == 'FR-PAC' else 0],
+    'region_FR-PDL': [1 if selected_region == 'FR-PDL' else 0],
+    'cos_heure': [np.cos(2 * np.pi * hour / 24)],
+    'sin_heure': [np.sin(2 * np.pi * hour / 24)],
+    'jour_sin': [np.sin(2 * np.pi * day / 365)],
+    'jour_cos': [np.cos(2 * np.pi * day / 365)],
+    'jour_semaine_sin': [np.sin(2 * np.pi * (date_input.weekday() + 1) / 7)],
+    'jour_semaine_cos': [np.cos(2 * np.pi * (date_input.weekday() + 1) / 7)],
+    'mois_sin': [np.sin(2 * np.pi * month / 12)],
+    'mois_cos': [np.cos(2 * np.pi * month / 12)]
+})
+    
+        # Bouton pour ajouter ou écraser les données
     if st.button("Prédire la consommation"):
         predicted_conso = model.predict(new_data)
+        # Affichage du résultat
         st.write(f"Consommation énergétique prédite: {predicted_conso[0]:.2f} MW")
+
+    # Affichage des données ajoutées
+    st.write("Données ajoutées:")
+    st.write(new_data)
